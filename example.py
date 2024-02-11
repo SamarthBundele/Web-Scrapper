e0c6cad65b4e6b91c592c583a63d8af41154a248
SamarthBundele <100133427+SamarthBundele@users.noreply.github.com>
2024-02-11 07:51:26 +0000

    data processing

--- example.py
+++ example.py
@@ -1,15 +1,33 @@
-# Import necessary libraries
 import requests
 from bs4 import BeautifulSoup 
+import pandas as pd 
 
-# The URL of the website you want to scrape
-url = "https://www.swiggy.com"
+url = "https://www.eazydiner.com/mumbai/restaurants"
 
-# Send a GET request to the website
 response = requests.get(url) 
 
-# Check if the request was successful
 if response.status_code == 200: 
     print("Success!")
 else:
     print("Error")
+
+data = []
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+restaurants = soup.find_all('div', class_="padding-10 radius-4 bg-white restaurant margin-b-10")
+for restaurant in restaurants: 
+    name = restaurant.find('h3', class_='grey res_name font-20 bold inline-block').text 
+    rating = restaurant.find('span', class_='critic').text 
+    print(name, rating)  
+    data.append({"Name": name, "Rating": rating})
+
+df = pd.DataFrame(data)
+df.to_csv("restaurants.csv", index=False)
+
+
+
+
+
+
+        
